0b66217a4a61fffeb062c634b9bc80c9fbce825d
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-07-12 15:44:19 +0200

    initial flask setup

---
+++ graderx/graders/manager.py
@@ -0,0 +1,79 @@
+from werkzeug.datastructures import FileStorage
+from pathlib import Path
+import shlex
+import subprocess
+import os
+## dependencies for docker
+import patoolib
+import glob
+import shutil
+# pip install patool
+
+
+def clean_directory(dir):
+    files = dir.glob('*')
+    for f in files:
+        if f.is_dir():
+            shutil.rmtree(str(f))
+        else:
+            f.unlink()
+
+def extract_file(file_path, verbosity=-1):
+    patoolib.extract_archive(file_path, outdir=file_path.parent, verbosity=verbosity)
+    os.remove(file_path)
+    return True
+
+def extract_submissions(dest_directory: Path, submissions_file: FileStorage,  verbosity= 0):
+    """
+    Args:
+    dest_directory: Path. Submission directory found in [lab_name]/config.py
+    submissions_file: FileStorage. It is used by the request object to represent uploaded files. 
+ 
+    Returns: bool
+    status: True on sucessful extraction
+
+    Actions:
+    extracts the submissions file in the destenation directory and removes the rar (or Whatever) file
+
+    """
+    
+    file_name = submissions_file.filename
+    # clean the submissions directory, if it doesn't exist create it along with missing parents
+    if dest_directory.exists():
+        clean_directory(dest_directory)
+    else:
+        dest_directory.mkdir(parents=True)
+
+    submissions_file.save(dst=(dest_directory / file_name))
+    file_path = dest_directory.joinpath(file_name)
+    # check if exists
+    try:
+        status = [extract_file(file_path), False][os.path.exists(file_path)]
+        print("***[Success]: File extracted successfully")
+    except:
+        print("***[Error]: Archive is damaged")
+        return False
+    return status
+
+
+
+def run_grader_commands(lab_id):
+    curr_dir = str(Path(__file__).parent.resolve())
+    cmd = shlex.split(f"pytest -vv --tb=short --show-capture=no {curr_dir}/courses/cc451/app/{lab_id}/test_run_grader.py")
+    file_name = "output.txt"
+    with open(file_name, "w+") as f:
+        subprocess.run(cmd, stdout=f)
+    parser_file = "parser_output"
+    lab_number = lab_id.split('lab')[-1]
+    with open(file_name, "r") as fi:
+        with open(parser_file, "w+") as fo:
+            cmd = shlex.split(f"python {curr_dir}/courses/cc451/app/lib/console_log_parser.py {lab_number} {curr_dir}/courses/cc451/app/res/{lab_id}")
+            subprocess.run(cmd, stdin=fi, stdout=fo)
+
+
+def run_grader(lab_id: str, submissions_file: FileStorage) -> dict:
+    curr_dir = str(Path(__file__).parent.resolve())
+    extract_submissions(Path(f"{curr_dir}/courses/cc451/app/{lab_id}/submissions/2020"), submissions_file)
+    run_grader_commands(lab_id)
+    return True
+    
---
+++ graderx/test_views.py
@@ -0,0 +1,100 @@
+from graderx import app
+import pytest
+from graderx.graders import manager
+from graderx import views
+import shutil
+import random
+import string
+from pathlib import Path
+import flask
+from io import BytesIO
+
+
+def generate_random_code():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits)
+                   for _ in range(12))
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+
+
+def test_receives_available_labs(client):
+    """
+    Test if the client receives the currently available labs
+    """
+    rv = client.get('/labs/cc451')
+    assert rv.json['labs'] == views.AVAILABLE_LABS
+
+
+def test_receives_correct_lab_results(client):
+    lab_id = generate_random_code()
+    path_to_result = Path(__file__).parent / \
+        f'graders/courses/cc451/app/res/{lab_id}'
+    path_to_result.parent.mkdir(exist_ok=True)
+    with path_to_result.open("w") as f:
+        f.write("Hello, World")
+    rv = client.get(f'/results/cc451/{lab_id}')
+    assert rv.get_data(as_text=True) == "Hello, World"
+    path_to_result.unlink()
+
+
+def test_receives_proper_status_if_results_not_found(client):
+    lab_id = generate_random_code()
+    rv = client.get(f"/results/cc451/{lab_id}")
+    assert rv.status_code == 404
+
+def test_status_unavailable_lab_provided(client):
+    lab_id = generate_random_code()
+    rv = client.post(f"/results/cc451/{lab_id}")
+    assert rv.json['status'] == views.UPLOAD_STATUS.LAB_NOT_EXIST.value(lab_id) and rv.status_code == 404
+
+
+def test_status_no_file_key_is_sent(client):
+    lab_id = views.AVAILABLE_LABS[0]
+    rv = client.post(f"/results/cc451/{lab_id}")
+    assert rv.json['status'] == views.UPLOAD_STATUS.FILE_NOT_INCLUDED.value and rv.status_code == 400
+
+
+def test_status_no_file_included(client):
+    lab_id = views.AVAILABLE_LABS[0]
+    rv = client.post(f"/results/cc451/{lab_id}", buffered=True,
+                           content_type='multipart/form-data',
+                           data={'submissions_file' : (BytesIO(), '')})
+    assert rv.json['status'] == views.UPLOAD_STATUS.FILE_NOT_SELECTED.value and rv.status_code == 400
+
+
+def test_status_file_unsupported(client):
+    lab_id = views.AVAILABLE_LABS[0]
+    rv = client.post(f"/results/cc451/{lab_id}", buffered=True,
+                           content_type='multipart/form-data',
+                           data={'submissions_file' : (BytesIO(b'hello, world'), 'file.txt')})
+    assert rv.json['status'] == views.UPLOAD_STATUS.UNSUPPORTED_FILE.value and rv.status_code == 400
+
+def test_status_failed_grading(monkeypatch, client):
+    def mock_run_grader(lab_id, submissions_file):
+        return False
+    
+    monkeypatch.setattr(manager, "run_grader",mock_run_grader)
+    lab_id = views.AVAILABLE_LABS[0]
+    rv = client.post(f"/results/cc451/{lab_id}", buffered=True,
+                           content_type='multipart/form-data',
+                           data={'submissions_file' : (BytesIO(b'my file contents'), 'myfile.rar')})
+    assert rv.json['status'] == views.UPLOAD_STATUS.GRADER_FAILED.value
+
+
+def test_status_successful_grading(monkeypatch, client):
+    def mock_run_grader(lab_id, submissions_file):
+        return True
+    
+    monkeypatch.setattr(manager, "run_grader",mock_run_grader)
+    lab_id = views.AVAILABLE_LABS[0]
+    rv = client.post(f"/results/cc451/{lab_id}", buffered=True,
+                           content_type='multipart/form-data',
+                           data={'submissions_file' : (BytesIO(b'my file contents'), 'myfile.rar')})
+    assert rv.json['status'] == views.UPLOAD_STATUS.SUCCESS.value
+
