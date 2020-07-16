a8bcdbcd7f9ce7c9d61949cecac0dc8002ac76d7
Ahmed Dewidar <a.mo.dewidar@gmail.com>
2020-07-16 06:33:08 +0200

    download submissions file from client and upload results

--- graderx/graders/manager.py
+++ graderx/graders/manager.py
@@ -3,21 +3,31 @@ from pathlib import Path
 import shlex
 import subprocess
 
+def extract_submissions(dest_directory: Path, submissions_file: FileStorage):
+    pass
 
-def get_results(course_code: str, lab_id: str) -> dict:
+def run_grader_commands(course_code, lab_id):
     curr_dir = str(Path(__file__).parent.resolve())
-    print(curr_dir)
     cmd = shlex.split(f"pytest -vv --tb=short --show-capture=no {curr_dir}/courses/{course_code}/app/{lab_id}/test_run_grader.py")
     file_name = "output.txt"
     with open(file_name, "w+") as f:
         subprocess.run(cmd, stdout=f)
     parser_file = "parser_output"
+    lab_number = lab_id.split('lab')[-1]
     with open(file_name, "r") as fi:
         with open(parser_file, "w+") as fo:
-            cmd = shlex.split(f"python {curr_dir}/courses/cc451/app/lib/console_log_parser.py 1_client")
+            cmd = shlex.split(f"python {curr_dir}/courses/cc451/app/lib/console_log_parser.py {lab_number} {curr_dir}/courses/cc451/app/res/{lab_id}")
             subprocess.run(cmd, stdin=fi, stdout=fo)
-    # TODO-TICKET-139:should return the results file
-    return "GRADED"
+
+
+def run_grader(course_code: str, lab_id: str, submissions_file: FileStorage) -> dict:
+    curr_dir = str(Path(__file__).parent.resolve())
+    try:
+        extract_submissions(Path(f"{curr_dir}/courses/{course_code}/app/{lab_id}/submissions/2020"), submissions_file)
+    except:
+        return False
+    run_grader_commands(course_code, lab_id)
+    return True
     
 
 def get_courses() -> list:
